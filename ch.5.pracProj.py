41dc180b4903cf243967df6bfa30be68a6446b69
Chris <surferbum618@ymail.com>
2019-03-04 20:08:49 -0600

    Ch.5 practice project almost complete, function is returning tuples

--- ch.5.pracProj.py
+++ ch.5.pracProj.py
@@ -43,6 +43,62 @@ def displayInventory(inventory):
     for k, v in inventory.items():
         item_total = item_total + inventory[k]
         #item_total = item_total + k.get(v, 0)
-    return print("Total number of items: " + str(item_total))
+    return "Total number of items: " + str(item_total)
 
 pprint.pprint(displayInventory(stuff))
+
+"""
+
+Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
+
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+Write a function named addToInventory(inventory, addedItems), where the inventory parameter is a dictionary representing the player’s inventory (like in the previous project) and the addedItems parameter is a list like dragonLoot. The addToInventory() function should return a dictionary that represents the updated inventory. Note that the addedItems list can contain multiples of the same item. Your code could look something like this:
+
+
+def addToInventory(inventory, addedItems):
+    # your code goes here
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
+The previous program (with your displayInventory() function from the previous project) would output the following:
+
+
+Inventory:
+45 gold coin
+1 rope
+1 ruby
+1 dagger
+
+Total number of items: 48
+
+"""
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(inventory, addedItems):
+    item_total = 0
+    for k, v in inventory.items():
+        item_total = item_total + inventory[k]
+    for item in addedItems:
+        item_total = item_total + 1
+        if item in inventory.keys():
+            inventory[item] = inventory[item] + 1
+        else:
+            inventory.setdefault(item, 1)
+
+    print(inventory.items())
+    #for k, v in inventory.items():
+        #if k in addedItems:
+            #v = v + 1
+            #print(k + ": " + str(v))
+        #else:
+            #print(v)
+
+    #for addedItems in inventory.key():
+        #print(addedItems)
+
+
+addToInventory(inv, dragonLoot)
