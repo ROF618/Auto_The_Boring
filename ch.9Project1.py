0968e71df4172627a1d10054a6089e4984be90b5
Chris <surferbum618@ymail.com>
2019-06-12 17:07:13 -0500

    working on the first project for ch.9 but my pipe key is not working so Im switching to my desktop

---
+++ ch.9Project1.py
@@ -0,0 +1,6 @@
+#! python3
+   # renameDates.py - Renames filenames with American MM-DD-YYYY date format
+   # to European DD-MM-YYYY.
+
+import shutil, os, re
+
---
+++ syncthingCopier.py
@@ -0,0 +1,22 @@
+import shutil, os
+import send2trash
+
+os.chdir('/home/chris/Sync')
+
+shutil.copytree('WebDev', '/home/chris/SyncLaptop')
+
+#remember that shutil.move can overwrite whats in that the destinations directory
+#also remember that when flushing this program out use the the send2trash module as it will send the previous version to the trash can
+
+"""
+try this out in your spare time
+
+for foldername, subfolders, filenames in os.walk('/home/chris/PycharmProjects'):
+    print('The current folder is ' + foldername)
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + foldername + ": " + subfolder)
+    for filename in filenames:
+        print('FILE INSIDE ' + foldername + ': ' + filename)
+    print('')
+
+"""
