8ac5655b4768007402312ff4ccfb0cb49120a910
Chris <surferbum618@ymail.com>
2019-06-26 19:00:06 -0500

    still working on ch.9 practice project, close to resolving the copy error; the loop breaks when traversing the sub directories

--- ch.9PracProject.py
+++ ch.9PracProject.py
@@ -19,13 +19,65 @@ As an added challenge, write another program that can insert gaps into numbered 
 """
 
 
-import os, pyperclip
+import os, shutil
 #find how to get the files extensions from the folder traversing loop
 #extResults = []
+
 for folderName, subfolders, filenames in os.walk('/home/chris/Pictures'):
     for file in filenames:
-        splitString = str(file).split(sep='.')
-        if splitString.pop(1) == "jpg":
-            #need to copy the directory the file is currently located in and the use pyperclip.paste method to put file in new directory
-            pyperclip.copy(file)
+        if file.endswith(".jpg"):
+            #the copy function works for the files in the directory but for the subdirectories more logic needs to introduced
+            pathFile = os.path.join(folderName, file)
+            #it still get caught up in the sub directories
+            shutil.copy2(pathFile, '/home/chris/Pictures/test')
+
+
+
+
 #print(extResults)
+"""
+
+zhdabuzwbvr21.jpg
+Screenshot from 2019-04-05 15-45-21.png
+z6ickpul15r21.jpg
+Screenshot from 2019-04-05 15-45-02.png
+Screenshot from 2019-04-05 15-44-51.png
+dlk1s4by90331.jpg
+Screenshot from 2019-04-01 19-18-41.png
+a8g3xc0yhk821.jpg
+selim_surgery_center_logoA_bevel2-280x164.png
+Screenshot from 2019-04-05 15-45-05 - 1.png
+c5eec78aebc5ef7e5e6e005dc10e0625ff45673e.png
+Screenshot from 2019-04-05 15-45-32.png
+Screenshot from 2019-04-04 14-30-45.png
+czaqoqwicbr21.jpg
+du6.jpg
+YHwOakm.jpg
+face_10.9.2014.png
+5y1tslajv0e21.jpg
+Screenshot from 2019-04-04 14-31-15.png
+Screenshot from 2019-04-15 12-43-27.png
+Screenshot from 2019-04-05 15-44-48.png
+Screenshot from 2019-04-05 15-45-22.png
+WWjtVFN.jpg
+Screenshot from 2019-04-05 15-45-05.png
+wvnmgbvu1wt11.jpg
+Screenshot from 2019-04-05 15-45-00.png
+Screenshot from 2019-04-05 15-45-33.png
+Screenshot from 2019-04-04 14-31-47.png
+Screenshot from 2019-04-01 19-19-08.png
+Screenshot from 2019-04-05 15-45-31.png
+a8g3xc0yhk821.jpg
+VbmBwot.png
+czaqoqwicbr21.jpg
+du6.jpg
+1491860899517.jpg
+VbmBwot (copy).png
+3.jpg
+2.jpg
+6.jpg
+2019-05-07.jpg
+5.jpg
+4.jpg
+
+"""
